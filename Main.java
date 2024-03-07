4731b99a5ff7c4db9aeaf08990b47ab754f80234
Letícia Aiko <131884917+letaiko@users.noreply.github.com>
2024-03-06 22:07:41 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,69 +4,86 @@ import java.sql.SQLOutput;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Passageiro edna = new Passageiro();
+        Passageiro passageiro1 = new Passageiro("Edna","edna.moda@gmail.com","trovaoazul",998760909);
         Passageiro joca = new Passageiro();
 
-        Motorista nicolas = new Motorista();
-        Motorista beatriz = new Motorista();
 
-        Carro clio = new Carro();
-        Carro kicks = new Carro();
+        Motorista motorista1 = new Motorista("Claudia","Cnh Ear", 4,"claudinha@gmail.com", "solamarelo", "Curitiba", "viagens");
+        Motorista motorista2 = new Motorista();
 
-        Pagamento cartao = new Pagamento();
-        Pagamento pix = new Pagamento();
-
-
-        clio.modelo = "clio";
-        clio.ano = 2006;
-        clio.cor = "grafiti";
-        clio.placa ="AUU0403";
-        clio.marca = "Renault";
-
-        kicks.modelo = "kicks";
-        kicks.ano = 2018;
-        kicks.cor = "preto";
-        kicks.placa = "BEE2114";
-        kicks.marca = "Nissan";
+        Carro carro1 = new Carro("Clio",2009,"Cinza","AUU0902","Renault");
+        Carro carro2 = new Carro();
 
 
 
-        nicolas.nome = "Nicolas";
-        nicolas.email = "nicolas.machado@gmail.com";
-        nicolas.senha = "32951096N";
-        nicolas.cidade = "Curitiba";
-        nicolas.tipoCnh = "EAR CNH";
-        nicolas.tipoCorrida = "Viagens";
-        nicolas.avaliacaoM = 4;
 
-        beatriz.nome = "Beatriz";
-        beatriz.email = "bea.miranda@gmail.com";
-        beatriz.senha = "olamundo21";
-        beatriz.cidade = "Castro";
-        beatriz.tipoCnh = "EAR CNH";
-        beatriz.tipoCorrida = "Viagens e Entregas";
-        beatriz.avaliacaoM = 5;
 
-        edna.nome = "edna";
-        edna.email = "ednamota@gmail.com";
-        edna.celular = 962821098;
-        edna.senha = "solazul";
 
-        joca.nome = "joca";
-        joca.email = "jocasilva@gmail.com";
-        joca.celular = 987654321;
-        joca.senha = "raioverde";
 
-        System.out.println("*****CONFIRMAÇÃO DE DADOS*****");
-        System.out.println("Seu nome é "+ edna.nome);
-        System.out.println("Seu e-mail é "+ edna.email);
-        System.out.println("Seu número de celular é "+ edna.celular);
 
-        System.out.println("*****CONFIRMAÇÃO DE DADOS*****");
-        System.out.println("Seu nome é "+ beatriz.nome);
-        System.out.println("Seu e-mail é "+ beatriz.email);
-        System.out.println("Você é de "+ be);
 
+
+
+
+        //Motorista beatriz = new Motorista();
+
+        //Carro carro1 = new Carro(clio, 2006);
+       //Carro kicks = new Carro();
+
+        //Pagamento cartao = new Pagamento();
+        //Pagamento pix = new Pagamento();
+
+
+//        clio.modelo = "clio";
+//        clio.ano = 2006;
+//        clio.cor = "grafiti";
+//        clio.placa ="AUU0403";
+//        clio.marca = "Renault";
+
+//        kicks.modelo = "kicks";
+//        kicks.ano = 2018;
+//        kicks.cor = "preto";
+//        kicks.placa = "BEE2114";
+//        kicks.marca = "Nissan";
+
+
+
+//        nicolas.nome = "Nicolas";
+//        nicolas.email = "nicolas.machado@gmail.com";
+//        nicolas.senha = "32951096N";
+//        nicolas.cidade = "Curitiba";
+//        nicolas.tipoCnh = "EAR CNH";
+//        nicolas.tipoCorrida = "Viagens";
+//        nicolas.avaliacaoM = 4;
+//
+//        beatriz.nome = "Beatriz";
+//        beatriz.email = "bea.miranda@gmail.com";
+//        beatriz.senha = "olamundo21";
+//        beatriz.cidade = "Castro";
+//        beatriz.tipoCnh = "EAR CNH";
+//        beatriz.tipoCorrida = "Viagens e Entregas";
+//        beatriz.avaliacaoM = 5;
+
+//        edna.nome = "edna";
+//        edna.email = "ednamota@gmail.com";
+//        edna.celular = 962821098;
+//        edna.senha = "solazul";
+//
+//        joca.nome = "joca";
+//        joca.email = "jocasilva@gmail.com";
+//        joca.celular = 987654321;
+//        joca.senha = "raioverde";
+//
+//        System.out.println("*****CONFIRMAÇÃO DE DADOS*****");
+//        System.out.println("Seu nome é "+ edna.nome);
+//        System.out.println("Seu e-mail é "+ edna.email);
+//        System.out.println("Seu número de celular é "+ edna.celular);
+//
+//        System.out.println("*****CONFIRMAÇÃO DE DADOS*****");
+//        System.out.println("Seu nome é "+ beatriz.nome);
+//        System.out.println("Seu e-mail é "+ beatriz.email);
+//        System.out.println("Você é de "+ beatriz.cidade);
+//        System.out.println();
 
     }
 }
