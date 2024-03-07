321e7a215f6c341a0237968dd1a7eb6a51bb0334
Letícia Aiko <131884917+letaiko@users.noreply.github.com>
2024-03-06 22:08:53 -0300

    Update Passageiro.java

--- Passageiro.java
+++ Passageiro.java
@@ -1,8 +1,24 @@
 public class Passageiro {
-    public String nome;
-    public String email;
-    public String senha;
-    public int celular;
+    private String nome;
+    private String email;
+    private String senha;
+    private int celular;
+
+    public Passageiro(String nome, String email, String senha, int celular){
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.celular = celular;
+    }
+
+    public Passageiro(){
+        this("Joca","joca.silva","jo2345",976554323);
+    }
+
+
+
+
+
 
     public void chamarCorrida(){
 
