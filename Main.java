351111ed2630f1927ce461fc76d42636a2885ece
gabivra21 <162526494+gabivra21@users.noreply.github.com>
2024-04-02 10:48:17 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,107 +1,55 @@
 import java.sql.SQLOutput;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        Passageiro passageiro1 = new Passageiro("Edna","edna.moda@gmail.com","trovaoazul",998760909);
-        Passageiro passageiro2 = new Passageiro();
+        Passageiro passageiro1 = new Passageiro("Edna", "edna.moda@gmail.com", "trovaoazul", 998760909, 4.9f);
+        Passageiro passageiro2 = new Passageiro("Eduardo", "edu.ardo@gmail.com", "9876alho", 999876523, 5.0f);
 
 
-        Motorista motorista1 = new Motorista("Claudia","Cnh Ear", 4,"claudinha@gmail.com", "solamarelo", "Curitiba", "viagens");
-        Motorista motorista2 = new Motorista();
+        Motorista motorista1 = new Motorista("Claudia", "Cnh Ear", 4.0f, "claudinha@gmail.com", "solamarelo", "Curitiba", "viagens", 992341569);
+        Motorista motorista2 = new Motorista("Kecio", "Cnh Ear", 3.9f, "kecio.silva@hotmail.com", "yolanda78", "Maringá", "viagens e entregas", 976543267);
 
-        Carro carro1 = new Carro("Clio",2009,"Cinza","AUU0902","Renault");
-        Carro carro2 = new Carro();
+        Carro carro1 = new Carro("Clio", 2009, "Cinza", "AUU0902", "Renault");
+        Carro carro2 = new Carro("Kicks", 2018, "preto", "BEE2114", "Nissan");
 
-        Pagamento pagamento1 = new Pagamento(true,false,false,false,false,89,6);
+        Moto moto1 = new Moto("Biz", "vermelho", "Honda", 2014, "PIA45BB");
+        Moto moto2 = new Moto("Harley", "Verde", "Harley-Davidson", 2018, "OOI-9876");
+
+
+        Pagamento pagamento1 = new Pagamento(true, false, false, false, false, 89, 6);
         Pagamento pagamento2 = new Pagamento();
 
         System.out.println("CONFIRMAÇÃO DADOS PASSAGEIRO 1");
-        System.out.println("Seu nome é: "+passageiro1.getNome());
-        System.out.println("Seu celular é: "+passageiro1.getCelular());
+        System.out.println("Seu nome é: " + passageiro1.nome);
+        System.out.println("Seu celular é: " + passageiro1.celular);
         System.out.println(" ");
         System.out.println("ALTERAÇÃO DE DADOS PASSAGEIRO 2");
-        System.out.println("Seu antigo nome :"+passageiro2.getNome());
+        System.out.println("Seu antigo nome :" + passageiro2.nome);
         passageiro2.setNome("Jocacio");
-        System.out.println("Seu novo nome é :"+passageiro2.getNome());
+        System.out.println("Seu novo nome é :" + passageiro2.nome);
         System.out.println(" ");
         System.out.println("VERIFICANDO AVALIAÇÃO DO MOTORISTA 1");
-        System.out.println("A avaliação do motorista 1 é "+ motorista1.getAvaliacaoM()+" estrelas");
+        System.out.println("A avaliação do motorista 1 é " + motorista1.avaliacao + " estrelas");
         System.out.println(" ");
         System.out.println("ALTERAÇÃO DE PREÇO NO PAGAMENTO!!!");
-        System.out.println("O antigo valor era: "+pagamento1.getPreco());
+        System.out.println("O antigo valor era: " + pagamento1.getPreco());
         pagamento1.setPreco(57);
-        System.out.println("O novo valor está: "+pagamento1.getPreco());
+        System.out.println("O novo valor está: " + pagamento1.getPreco());
 
-
-
-
-
-
-
-
-
-
-        //Motorista beatriz = new Motorista();
-
-        //Carro carro1 = new Carro(clio, 2006);
-       //Carro kicks = new Carro();
-
-        //Pagamento cartao = new Pagamento();
-        //Pagamento pix = new Pagamento();
-
-
-//        clio.modelo = "clio";
-//        clio.ano = 2006;
-//        clio.cor = "grafiti";
-//        clio.placa ="AUU0403";
-//        clio.marca = "Renault";
-
-//        kicks.modelo = "kicks";
-//        kicks.ano = 2018;
-//        kicks.cor = "preto";
-//        kicks.placa = "BEE2114";
-//        kicks.marca = "Nissan";
-
-
-
-//        nicolas.nome = "Nicolas";
-//        nicolas.email = "nicolas.machado@gmail.com";
-//        nicolas.senha = "32951096N";
-//        nicolas.cidade = "Curitiba";
-//        nicolas.tipoCnh = "EAR CNH";
-//        nicolas.tipoCorrida = "Viagens";
-//        nicolas.avaliacaoM = 4;
-//
-//        beatriz.nome = "Beatriz";
-//        beatriz.email = "bea.miranda@gmail.com";
-//        beatriz.senha = "olamundo21";
-//        beatriz.cidade = "Castro";
-//        beatriz.tipoCnh = "EAR CNH";
-//        beatriz.tipoCorrida = "Viagens e Entregas";
-//        beatriz.avaliacaoM = 5;
-
-//        edna.nome = "edna";
-//        edna.email = "ednamota@gmail.com";
-//        edna.celular = 962821098;
-//        edna.senha = "solazul";
-//
-//        joca.nome = "joca";
-//        joca.email = "jocasilva@gmail.com";
-//        joca.celular = 987654321;
-//        joca.senha = "raioverde";
-//
-//        System.out.println("*****CONFIRMAÇÃO DE DADOS*****");
-//        System.out.println("Seu nome é "+ edna.nome);
-//        System.out.println("Seu e-mail é "+ edna.email);
-//        System.out.println("Seu número de celular é "+ edna.celular);
-//
-//        System.out.println("*****CONFIRMAÇÃO DE DADOS*****");
-//        System.out.println("Seu nome é "+ beatriz.nome);
-//        System.out.println("Seu e-mail é "+ beatriz.email);
-//        System.out.println("Você é de "+ beatriz.cidade);
-//        System.out.println();
+        System.out.println("Características da Moto 1!!!");
+        System.out.println("Sua marca é: " + moto1.marca);
+        System.out.println("Sua cor é: " + moto1.cor);
+        System.out.println("Seu modelo é: " + moto1.modelo);
 
     }
 }
+
+
+
+
+
+
+
+
+
