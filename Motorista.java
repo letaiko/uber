c3fc972b43d51e959150fcb9e41c3680a358dd58
gabivra21 <162526494+gabivra21@users.noreply.github.com>
2024-04-02 10:49:02 -0300

    Update Motorista.java

--- Motorista.java
+++ Motorista.java
@@ -1,42 +1,27 @@
-public class Motorista {
+public class Motorista extends Pessoa {
 
-    private String nome;
+
     private String tipoCnh;
-    private int avaliacaoM;
-    private String email;
-    private String senha;
+
     private String cidade;
     private String tipoCorrida;
 
-    public Motorista(String nome, String tipoCnh,int avaliacaoM,String email, String senha, String cidade, String tipoCorrida){
-        this.nome = nome;
+    public Motorista(String nome, String tipoCnh, float avaliacao,String email, String senha, String cidade, String tipoCorrida, int celular){
+        super(nome, email, senha,celular, avaliacao);
         this.tipoCnh = tipoCnh;
-        this.avaliacaoM = avaliacaoM;
-        this.email = email;
-        this.senha = senha;
         this.cidade = cidade;
         this.tipoCorrida = tipoCorrida;
        }
 
-    public Motorista(){
-        this("Marcelo","Cnh Ear",5,"marcelo.pereira@gmail.com","ceuazul","Londrina","Entregas");
-    }
 
-    public String getNome(){
-        return nome;
-    }
 
-    public int getAvaliacaoM(){
-        return avaliacaoM;
-    }
+
+
 
     public String getTipoCorrida(){
         return tipoCorrida;
     }
 
-    public void setAvaliacaoM(int avaliacaoM){
-        this.avaliacaoM = avaliacaoM;
-    }
 
     public void aceitarCorrida(){
 
